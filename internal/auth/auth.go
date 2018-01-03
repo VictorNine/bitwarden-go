VictorNine/bitwarden-go

commit a2b25c9131e31578519365b2e2b4b5c7c4220c93

Change signinkey to use a string instead of a byte array

internal/auth/auth.go
@@ -21,11 +21,11 @@ import (
 
 type Auth struct {
 	db         database
-	signingKey []byte
+	signingKey string
 	jwtExpire  int
 }
 
-func New(db database, signingKey []byte, jwtExpire int) Auth {
+func New(db database, signingKey string, jwtExpire int) Auth {
 	auth := Auth{
 		db:         db,
 		signingKey: signingKey,

internal/auth/auth_test.go
@@ -23,7 +23,7 @@ func TestHandleLogin(t *testing.T) {
 
 	keyHash, _ := reHashPassword("sjlcxv1TSe1wTHoYF50WJL3X07oCFxqhXYFeGfrbtII=", "nobody@example.com")
 	db := &mock.MockDB{Username: "nobody@example.com", Password: keyHash, RefreshToken: "abcdef"}
-	authHandler := New(db, nil, 3600)
+	authHandler := New(db, "", 3600)
 
 	for _, c := range cases {
 		req, err := http.NewRequest("POST", "/identity/connect/token", strings.NewReader(c.data.Encode()))

commit 7ebdbf43973670a7f03ec57d8c624a7f0582f5da

fixed bug caused by a2b25c9131e31578519365b2e2b4b5c7c4220c93

internal/auth/auth.go
@@ -21,14 +21,14 @@ import (
 
 type Auth struct {
 	db         database
-	signingKey string
+	signingKey []byte
 	jwtExpire  int
 }
 
 func New(db database, signingKey string, jwtExpire int) Auth {
 	auth := Auth{
 		db:         db,
-		signingKey: signingKey,
+		signingKey: []byte(signingKey),
 		jwtExpire:  jwtExpire,
 	}
 