907c92ac5fe667e41f0434b1997a41e9d6293873
Ryan Hoeck <ryan.hoeck.01@gmail.com>
2018-01-03 01:21:09 -0500

    Adding initial basic tests for config_test.go

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,10 +1,21 @@
 package config
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"strconv"
-	"strings"
 	"testing"
 )
+
+func TestHasColons(t *testing.T) {
+	containsColon := hasColon(":8000")
+	if containsColon == false {
+		t.Errorf("hasColon returned false when string has colon. Want: true")
+	}
+	containsColon = hasColon("8000")
+	if containsColon == true {
+		t.Errorf("hasColon returned true when string does not have a colon. Want: false")
+	}
+
+}
+
+func TestReadConfiguration(t *testing.T) {
+
+}
